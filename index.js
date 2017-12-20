0dd5f7fe657ab7f1c82eb6f924f651f35bb9b8ad
David Mendelovits <david.mendelovits@gmail.com>
2017-12-20 06:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
   recipes[key] = value;
   return recipes
 }
+
+function deleteFromObjectByKey(recipes, key) {
+  var newObj = Object.assign( {}, recipes);
+  delete newObj[key]
+}
